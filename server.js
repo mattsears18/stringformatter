eadd6ff61ae2943257bb8716b2da30bc5b531aba
Matt Sears <mattsears@Matts-MacBook-Pro.local>
2016-12-29 14:12:46 -0500

    db

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ var db;
 MongoClient.connect(process.env.MONGODB_URI || 'mongodb://heroku_3zwvsqsq:446onvqsjjanf81skjhmf51it4@ds149278.mlab.com:49278/heroku_3zwvsqsq', (err, database) => {
   if (err) return console.log(err);
   db = database;
-  app.listen(3000, () => {
-    console.log('listening on 3000');
+
+  port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log('listening on ' + port);
   });
 });
 
